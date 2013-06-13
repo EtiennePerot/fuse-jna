b2278ce8c167de6aa11675fafef1dfcd898df41c
Andrew Kiellor <akiellor@gmail.com>
2013-06-13 13:41:12 -0700

    Nicer message when gradle isn't installed.

--- examples/run.sh
+++ examples/run.sh
@@ -6,6 +6,12 @@ if [ $# -lt 2 ]; then
 	echo "Usage: $0 full.class.name [fs-arguments] mountpoint" >&2
 	exit 1
 fi
+
+if ! which gradle &> /dev/null; then
+  echo 'gradle not found in $PATH. Please install gradle.' >&2
+  exit 1
+fi
+
 cd "`dirname "$0"`/.."
 mountPoint="${@: -1}"
 if [ ! -d "$mountPoint" ]; then
