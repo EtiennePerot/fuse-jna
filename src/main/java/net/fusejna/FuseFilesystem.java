b41b31981b107d41f2da33a305694cfc514eb2fd
Etienne Perot <etienne@perot.me>
2014-03-17 20:27:28 -0700

    Finish incomplete docstring for defaultStat. Avoid getting the UID/GID twice.

--- src/main/java/net/fusejna/FuseFilesystem.java
+++ src/main/java/net/fusejna/FuseFilesystem.java
@@ -364,16 +364,19 @@ public abstract class FuseFilesystem
 
 	/**
 	 * Subclasses may override this to customize the default parameters applied to the stat structure, or to prevent such
-	 * behavior (by overriding this method with an empty one)
+	 * behavior in the first place (by overriding this method with an empty one).
 	 * 
-	 * @param stat
-	 *            The
+	 * @param wrapper
+	 *            The StatWrapper object to write to.
+	 * @param uid
+	 *            The UID under which the JVM is running.
+	 * @param gid
+	 *            The GID under which the JVM is running.
 	 */
 	protected void defaultStat(final StatWrapper wrapper, final long uid, final long gid)
 	{
 		// Set some sensible defaults
-		wrapper.setMode(NodeType.DIRECTORY).setAllTimesMillis(System.currentTimeMillis()).nlink(1).uid(FuseJna.getUid())
-				.gid(FuseJna.getGid());
+		wrapper.setMode(NodeType.DIRECTORY).setAllTimesMillis(System.currentTimeMillis()).nlink(1).uid(uid).gid(gid);
 	}
 
 	@UserMethod
