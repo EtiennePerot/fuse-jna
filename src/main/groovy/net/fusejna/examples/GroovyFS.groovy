acf7fa7b5b099113059ac675a6921ed8ffc05325
grant haywood <grant@iowntheinter.net>
2014-12-22 00:33:40 -0800

    groovy support

---
+++ src/main/groovy/net/fusejna/examples/GroovyFS.groovy
@@ -0,0 +1,58 @@
+package net.fusejna.examples;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+
+import net.fusejna.DirectoryFiller;
+import net.fusejna.ErrorCodes;
+import net.fusejna.FuseException;
+import net.fusejna.StructFuseFileInfo.FileInfoWrapper;
+import net.fusejna.StructStat.StatWrapper;
+import net.fusejna.types.TypeMode.NodeType;
+import net.fusejna.util.FuseFilesystemAdapterFull;
+
+public class GroovyFS extends FuseFilesystemAdapterFull
+{
+	public static void main(final String... args) throws FuseException
+	{
+		if (args.length != 1) {
+			System.err.println("Usage: GroovyFS <mountpoint>");
+			System.exit(1);
+		}
+		new GroovyFS().log(true).mount(args[0]);
+	}
+
+	final String filename = "/hello.txt";
+	final String contents = "Hello World!\n";
+
+	@Override
+	public int getattr(final String path, final StatWrapper stat)
+	{
+		if (path.equals(File.separator)) { // Root directory
+			stat.setMode(NodeType.DIRECTORY);
+			return 0;
+		}
+		if (path.equals(filename)) { // hello.txt
+			stat.setMode(NodeType.FILE).size(contents.length());
+			return 0;
+		}
+		return -ErrorCodes.ENOENT();
+	}
+
+	@Override
+	public int read(final String path, final ByteBuffer buffer, final long size, final long offset, final FileInfoWrapper info)
+	{
+		// Compute substring that we are being asked to read
+		final String s = contents.substring((int) offset,
+				(int) Math.max(offset, Math.min(contents.length() - offset, offset + size)));
+		buffer.put(s.getBytes());
+		return s.getBytes().length;
+	}
+
+	@Override
+	public int readdir(final String path, final DirectoryFiller filler)
+	{
+		filler.add(filename);
+		return 0;
+	}
+}
